498c20a18ed6ba88db56299ecc1f0a3582b5bd27
Kyaw Wanna <mgkyawwanna@gmail.com>
2026-03-04 23:08:47 +0630

    fix: removed decoy text and wired App.tsx to load the main router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { useRoutes } from "react-router-dom";
-import { routes } from "./routes/routes";
+import React from 'react';
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { AuthProvider } from './state/auth';
+import { LanguageProvider } from './contexts/LanguageContext';
+import { routes } from './routes/routes';
+
+// This function injects the massive routing file we fixed earlier
+function AppRouter() {
+  return useRoutes(routes);
+}
 
 export default function App() {
-  const content = useRoutes(routes);
-  return <div className="antialiased min-h-screen">{content}</div>;
+  return (
+    <LanguageProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </AuthProvider>
+    </LanguageProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import { I18nProvider } from "./state/i18n";
-import { AuthProvider } from "./state/auth";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter 
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <I18nProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </I18nProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
